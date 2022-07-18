0f24ebe7c8b2e65ad76511b088dc789566f68d6a
Itsgotime45 <33234003+Itsgotime45@users.noreply.github.com>
2022-07-17 20:52:05 -0500

    Update

--- C++Practice/Source.cpp
+++ C++Practice/Source.cpp
@@ -9,7 +9,7 @@ int main()
 	return 0;
 }
 */
-
+/*------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
 /* Variables
 int main()
 {
@@ -30,7 +30,7 @@ int main()
 
 }
 */
-
+/*------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
 /* Global and local Vars
 int x = 9, w = 6;
 float z; 
@@ -49,6 +49,7 @@ int main()
 
 }
 */
+/*------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
 /* User input - Made several mistakes
 * try one
 * int main()
@@ -68,7 +69,7 @@ int main()
     cout << "You entered:" << x << "and" << y << endl; --- there was no spaces after "You entered:" and none before and after "and" result in "You entered 5and3" 
     return 0;
 } 
-*Try 2 and the correct one
+*Try 2 and the correct one and correct prints N and N 
 int main()
 
 {
@@ -87,3 +88,88 @@ int main()
 	return 0;
 }
 */
+/*------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+/* Strings
+int main()
+{
+	char STR1[200];
+	cout << "String Input \n" << endl;
+
+	cout << "Insert some strings:" << endl;
+
+	cin.getline(STR1, 200);
+
+	cout << "You entered:" << STR1 << endl;
+}
+*/
+/*------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+/*Adding numbers
+int main()
+{
+	int x, y, sum;
+	cout << "Addition of two numbers\n" << endl;
+
+	cout << "Insert first number:" << endl;
+
+	cin >> x;
+
+	cout << "Insert sceond number" << endl; 
+
+	cin >> y; 
+
+	sum = x + y; 
+	cout << "The result of x + y =" << sum << endl;
+
+	return 0; 
+}
+*/
+/*------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+/* Math Operators
+int main() 
+{
+	int x, y, sum, mul, sub, mod; 
+	float div; 
+	cout << "Addition, Subtraction, Multiplication and Division of two numbers" << endl;
+	cout << "Insert first number";
+	cin >> x;
+	cout << "Insert second numbner:" << endl;
+	cin >> y;
+
+	sum = x + y;
+	mul = x * y;
+	sub = x - y;
+	mod = x % y;
+	div = x / y;
+
+	cout << "the sum of" << x << "+" << y << "=" << sum << endl;
+	cout << "TheMultication of x*y =" << mul << endl;
+	cout << "The subtraction of x - y =" << sub << endl;
+	cout << "The modulus x%y =" << mod << endl; 
+	cout << "The division of x/y =" << div << endl; 
+
+	return 0; 
+}
+*/
+/*Add N Numbers
+int main()
+{
+	int x,y,z, sum = 0; 
+	cout << "Adding N nUmbers \n" << endl;
+	cout << "Please choose range of number to add:" << endl;
+	cin >> x;
+
+	cout << "Please insert" << x << "number(s) only:" << endl;
+
+	for (y = 1; y <= x; y++)
+	{
+		cin >> z;
+		sum = sum + z;
+	}
+
+	cout << "the result of addition of" << x << "number(s) is=" << sum << endl;
+
+	return 0;
+}
+*/
+int main()
+
